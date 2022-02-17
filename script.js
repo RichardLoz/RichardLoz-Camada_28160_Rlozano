273b7fce44b4d770483fbfb13cd115deb7dc8b96
Richard Lozano <56285028+RichardLoz@users.noreply.github.com>
2022-02-17 06:31:04 -0300

    Add files via upload
    Desafio Complementario de ARRAY

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const calcularTotal = () => {
+/* const calcularTotal = () => {
   let total = 0;
   let subTotal;
 
   do {
-    subTotal = prompt("Ingrese el valor de sus productos");
+    subTotal = prompt(
+      `Ingrese el valor de sus productos, y "Total" para saber el precio final `
+    );
 
     const parsedSubTotal = +subTotal;
 
@@ -21,4 +23,25 @@ const calcularTotal = () => {
 
 const precioFinal = calcularTotal();
 
-alert(`El precio total de su compra es $ ${precioFinal}`);
+alert(`El precio total de su compra es $ ${precioFinal}`); */
+
+// TODO: Entrega complementaria - ARRAY -
+
+// Se solicita al usuario que ingrese la cantidad de productos que desea agregar al Stock y ese numero sera la cantidad de iteraciones que hara el ciclo.
+
+const agregarProducto = () => {
+  let cantProductos = parseInt(
+    prompt("Ingrese la cantidad de productos que desee agregar al inventario")
+  );
+  let stockGeneral = [];
+  let producto;
+
+  for (i = 0; i < cantProductos; i++) {
+    producto = prompt("Ingrese su producto");
+    stockGeneral.push(producto);
+  }
+
+  return stockGeneral;
+};
+const inventario = agregarProducto();
+console.table(inventario);
