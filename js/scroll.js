30def55a131dd0b763baf4a9fd2a5c4ca0c01b55
Richard Lozano <56285028+RichardLoz@users.noreply.github.com>
2022-03-21 21:07:34 -0300

    Add files via upload
    Se añade la librería Scroll Reveal

---
+++ js/scroll.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal();
+
+sr.reveal(".navbar", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "-100px",
+});
+
+sr.reveal(".productos", {
+  duration: 5000,
+  origin: "left",
+  distance: "400px",
+});
